8fd25d5d143827b53bd41da4a33c575cf867d01e
sandheather <heather.e.sanders22@gmail.com>
2016-03-19 10:45:49 -0400

    form function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 var $input = $('#to-do-input');
 var $list = $('.list');
+
+$('form').on('submit', function (e) {
+  var $li = $('<li>');
+
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+  $input.val('');
+});
+
